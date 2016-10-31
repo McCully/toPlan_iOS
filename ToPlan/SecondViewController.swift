962718791aa72c0ef42ec57036f6a7af2bae878e
Mat McCully <mccully1894@gmail.com>
2016-10-31 16:26:44 -0500

    Initial Commit

---
+++ ToPlan/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ToPlan
+//
+//  Created by Mathew Mccully on 10/31/16.
+//  Copyright © 2016 Mathew Mccully. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
